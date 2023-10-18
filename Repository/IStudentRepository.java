8ba755775d53ad2e1c31cac108bd3c28fc7570a7
levanhuy12345 <lehuy1234987@gmail.com>
2023-10-18 10:57:19 +0700

    Studentinfo

---
+++ Repository/IStudentRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Repository;
+
+import Model.Student;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC
+ */
+public interface IStudentRepository {
+    void display(ArrayList<Student> lt);
+}
